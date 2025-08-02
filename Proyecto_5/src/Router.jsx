3dd3f08198dd0299b75ba50a5b4a23c5c4c980ac
unknown <cami.rivas.dlb@gmail.com>
2025-08-01 21:31:58 -0400

    Se agrega nueva ruta ApodDetail

--- Proyecto_5/src/Router.jsx
+++ Proyecto_5/src/Router.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home.jsx';
-/* import {Asteroids} from './pages/Asteroids/Asteroids.jsx';
-import {SolarEvents} from './pages/SolarEvents/SolarEvents.jsx';
-import {Dashboard} from './pages/Dashboard/Dashboard.jsx';*/
+import Asteroids from './pages/Asteroids/Asteroids.jsx';
+ import SolarEvents from './pages/SolarEvents/SolarEvents.jsx';
+/*import {Dashboard} from './pages/Dashboard/Dashboard.jsx'; */
 import ApodImage from './pages/ApodImage/ApodImage.jsx'; 
 
 export default function AppRouter() {
@@ -10,10 +10,11 @@ export default function AppRouter() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
- {/*        <Route path="/asteroids" element={<Asteroids />} />
-        <Route path="/solar-events" element={<SolarEvents />} />
-        <Route path="/dashboard" element={<Dashboard />} />*/
-        <Route path="/apod-image" element={<ApodImage />} /> }
+        <Route path="/asteroids" element={<Asteroids />} />
+         {/* <Route path="/solar-events" element={<SolarEvents />} /> */}
+         <Route path="/apod/:date" element={<ApodDetail />} />
+      {/*  <Route path="/dashboard" element={<Dashboard />} /> */}
+        <Route path="/apod-image" element={<ApodImage />} /> 
       </Routes>
     </BrowserRouter>
   );
