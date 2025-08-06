81b1a5f68981be4e52b610f072bb2536f8227438
unknown <cami.rivas.dlb@gmail.com>
2025-08-05 22:28:10 -0400

    Actualización Rutas

--- Proyecto_5/src/Router.jsx
+++ Proyecto_5/src/Router.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home.jsx';
 import Asteroids from './pages/Asteroids/Asteroids.jsx';
 /*  import SolarEvents from './pages/SolarEvents/SolarEvents.jsx'; */
-import {Dashboard} from './pages/Dashboard/Dashboard.jsx'; 
+import {Dashboard} from './pages/Dashboard/AsteroidsDashboard.jsx';
 import ApodImage from './pages/ApodImage/ApodImage.jsx'; 
 import ApodDetail from './pages/ApodImage/ApodDetail.jsx';
 import Layout from './components/Layout/Layout.jsx';
@@ -16,8 +16,8 @@ export default function AppRouter() {
         <Route path="/asteroids" element={<Asteroids />} />
          {/* <Route path="/solar-events" element={<SolarEvents />} /> */}
          <Route path="/apod/:date" element={<ApodDetail />} />
-       <Route path="/dashboard" element={<Dashboard />} /> 
-        <Route path="/apod-image" element={<ApodImage />} /> 
+       <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/apod-image" element={<ApodImage />} />
         </Route>
       </Routes>
     </BrowserRouter>
