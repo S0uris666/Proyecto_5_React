da22497afb1627ce9384ecfc76b7ff6f5d5c3cd6
unknown <cami.rivas.dlb@gmail.com>
2025-08-05 22:27:29 -0400

    Error boundary

--- Proyecto_5/src/main.jsx
+++ Proyecto_5/src/main.jsx
@@ -1,27 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { fetchNeoFeed, fetchDonkiEvents } from './utils/api.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { fetchNeoFeed, fetchDonkiEvents } from "./utils/api.jsx";
+import ErrorBoundary from "./components/ErrorBoundary.jsx";
+import BrokenComponent from "./components/BrokenComponent.jsx";
 
 async function testApi() {
-  const startDate = '2025-07-20';
-  const endDate = '2025-07-25';
+  const startDate = "2025-07-20";
+  const endDate = "2025-07-25";
 
   try {
     const data = await fetchNeoFeed(startDate, endDate);
-    const data_DONKI= await fetchDonkiEvents('CME', startDate, endDate);
+    const data_DONKI = await fetchDonkiEvents("CME", startDate, endDate);
 
-    console.log(' Observaciones obtenidas:', data);
-    console.log(' Eventos DONKI obtenidos:', data_DONKI);
+    console.log(" Observaciones obtenidas:", data);
+    console.log(" Eventos DONKI obtenidos:", data_DONKI);
   } catch (err) {
-    console.error(' Error al consumir la API:', err.message);
+    console.error(" Error al consumir la API:", err.message);
   }
 }
 
 testApi();
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ErrorBoundary>
+      <App />
+      
+    </ErrorBoundary>
+  </StrictMode>
+);
