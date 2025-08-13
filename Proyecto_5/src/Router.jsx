0b19cdd068825bf6f04f1387ff9030e60e3b7988
unknown <cami.rivas.dlb@gmail.com>
2025-08-13 19:09:23 -0400

    Se elimina ruta Solar Events

--- Proyecto_5/src/Router.jsx
+++ Proyecto_5/src/Router.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home.jsx';
 import Asteroids from './pages/Asteroids/Asteroids.jsx';
-/*  import SolarEvents from './pages/SolarEvents/SolarEvents.jsx'; */
 import Dashboard from './pages/Dashboard/Dashboard.jsx';
 import ApodImage from './pages/ApodImage/ApodImage.jsx'; 
 import ApodDetail from './pages/ApodImage/ApodDetail.jsx';
@@ -14,7 +13,6 @@ export default function AppRouter() {
         <Route path="/" element={<Layout />} >
         <Route index element={<Home />} />
         <Route path="/asteroids" element={<Asteroids />} />
-         {/* <Route path="/solar-events" element={<SolarEvents />} /> */}
          <Route path="/apod/:date" element={<ApodDetail />} />
        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/apod-image" element={<ApodImage />} />
